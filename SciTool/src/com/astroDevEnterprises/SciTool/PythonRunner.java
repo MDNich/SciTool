695d2a193c6157cee511515ac411cb2d931a2d72
Astro Kid <71262643+MDNich@users.noreply.github.com>
2022-08-15 08:19:26 -0400

    current

--- SciTool/src/com/astroDevEnterprises/SciTool/PythonRunner.java
+++ SciTool/src/com/astroDevEnterprises/SciTool/PythonRunner.java
@@ -1,5 +1,4 @@
 package com.astroDevEnterprises.SciTool;
-import org.python.util.PythonInterpreter;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -8,41 +7,40 @@ import java.util.List;
 
 public class PythonRunner {
 
-    PythonInterpreter pyInterp;
+    String pathToPython = "/usr/local/bin/python3";
+
+    public PythonRunner(String pathToPython)
+    {
+        this.pathToPython = pathToPython;
+    }
     public PythonRunner()
     {
-        try {
-            pyInterp = new PythonInterpreter();
-        }
-        catch (Exception e) {
-
-        }
+        this.pathToPython = "/usr/local/bin/python3";
     }
    public void runContourGivenInputs(Object[] inputs)
    {
+       System.out.println(PythonRunner.class.getProtectionDomain().getCodeSource().getLocation());
        /*
        let arr = [PythonObject.StringLiteralType(dirPathSave),PythonObject.ArrayLiteralElement(ys),PythonObject.ArrayLiteralElement(xs),PythonObject.ArrayLiteralElement(qs),PythonObject.FloatLiteralType(Double(WindowLowerX.stringValue)!),PythonObject.FloatLiteralType(Double(WindowLowerY.stringValue)!),PythonObject.FloatLiteralType(Double(WindowUpperX.stringValue)!),PythonObject.FloatLiteralType(Double(WindowUpperY.stringValue)!),PythonObject.IntegerLiteralType(Int(CounterNumber.stringValue)!),PythonObject.FloatLiteralType(Double(CoulombConstant.stringValue)!)] as [PythonConvertible]
         try engine.drawGraph.throwing.dynamicallyCall(withArguments: arr)
        */
-       try(PythonInterpreter pyInterp = new PythonInterpreter()) {
-           pyInterp.exec("print('Hello Python World!')");
-       }
-       //pyInputs.add("C:\\Users\\marcd\\AppData\\Local\\Microsoft\\WindowsApps\\python3.exe");
-       //pyInputs.add("voltage.py");
+       ArrayList pyInputs = new ArrayList();
+       pyInputs.add(pathToPython);
+       pyInputs.add("com/astroDevEnterprises/SciTool/voltage.py");
        for(int i = 0; i < inputs.length; i++)
        {
-           //pyInputs.add(inputs.toString());
+           pyInputs.add(inputs.toString());
        }
-       //ProcessBuilder pb = new ProcessBuilder(pyInputs);
-       /*try {
-           //pb.directory(new File(PythonRunner.class.getProtectionDomain().getCodeSource().getLocation().toURI()));
+       ProcessBuilder pb = new ProcessBuilder(pyInputs);
+       try {
+           pb.directory(new File(PythonRunner.class.getProtectionDomain().getCodeSource().getLocation().toURI()));
        } catch (URISyntaxException e) {
            throw new RuntimeException(e);
        }
        try {
-           //Process p = pb.start();
+           Process p = pb.start();
        } catch (IOException e) {
            throw new RuntimeException(e);
-       }*/
+       }
    }
 }
