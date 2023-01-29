149947b22a95e58a12f6e84204ea157e1073e1bc
Astro Kid <71262643+MDNich@users.noreply.github.com>
2023-01-28 21:24:25 -0500

    some changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,9 @@ module org.scitool.scitool {
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires java.desktop;
+    requires nsmenufx;
+    requires jfa;
+    //requires de.jangassen;
 
     opens org.scitool to javafx.fxml;
     exports org.scitool;
--- src/main/java/org/scitool/SciTool.java
+++ src/main/java/org/scitool/SciTool.java
@@ -1,22 +1,32 @@
 package org.scitool;
 
+import de.jangassen.MenuToolkit;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+//import de.jangassen.MenuToolkit;
+import javafx.scene.control.MenuItem;
 
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URL;
+import java.util.Locale;
 
 public class SciTool extends Application {
 
     public static File logFile = null;
+
+    public JMenuBar menuBar = null;
 
     Stage stageMain = null;
 
@@ -24,7 +34,7 @@ public class SciTool extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         try {
-            URL iconURL = SciTool.class.getResource("Icon.png");
+
             File newFile = new File("/tmp/log_scitool.txt"); // create, or fetch if previously created.
             if (newFile.delete()) {
                 System.out.println("Deleted the old log file: " + newFile.getName());
@@ -37,23 +47,71 @@ public class SciTool extends Application {
             logger = new LogWriter(logFile);
             System.out.println("Fetched logfile.");
             logger.log("Running Start Method.");
-            Image image = new ImageIcon(iconURL).getImage();
-            com.apple.eawt.Application.getApplication().setDockIconImage(image);
+            //com.apple.eawt.Application.getApplication().setDockIconImage(image);
+
         } catch (Exception e) {
             System.out.println("Icon failed");
-            logger.log("Icon failed.");
+            logger.log("Start failed.");
 
             // Won't work on Windows or Linux.
         }
+        try {
+            final Taskbar taskbar = Taskbar.getTaskbar();
+            final Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+
+            try {
+                //set icon for mac os (and other systems which do support this method)
+                String currentPath = new java.io.File(".").getCanonicalPath();
+                logger.log("Current dir:" + currentPath);
+                File tmp = new File(currentPath + "/Icon.png");
+                Image image = ImageIO.read(tmp);// new ImageIcon(iconURL).getImage();
+                //getClass().getResource()
+                taskbar.setIconImage(image);
+                menuBar = new JMenuBar();
+            } catch (final UnsupportedOperationException e) {
+                System.out.println("The os does not support: 'taskbar.setIconImage'");
+            } catch (final SecurityException e) {
+                System.out.println("There was a security exception for: 'taskbar.setIconImage'");
+            }
+
+        }
+        catch (Exception e) {
+            logger.log(e.toString());
+            logger.log("Icon2 Failed.");
+        }
+        //MenuToolkit tk = MenuToolkit.toolkit(Locale.getDefault());
+        //tk.setApplicationMenu(tk.createDefaultApplicationMenu("test"));
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.useSystemMenuBarProperty().set(true);
+
+        Menu menu = new Menu("SciTool");
+        MenuItem item = new MenuItem("About SciTool");
+
+        Menu help = new Menu("Help");
+        menu.getItems().add(item);
+        menuBar.getMenus().addAll(menu, help);
+
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(SciTool.class.getResource("home-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());//, 320, 240);
+        Node fxmlOut = fxmlLoader.load();
+        BorderPane border = new BorderPane();
+        border.setTop(menuBar);
+        border.setCenter(fxmlOut);
+        Scene scene = new Scene(border);//, 320, 240);
         stage.setTitle("Welcome!");
         stage.initStyle(StageStyle.DECORATED);
+
+
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
         stageMain = stage;
+        System.setProperty( "apple.awt.application.name", "SciTool" );
         logger.log("Show scene succeeded.");
+
+
     }
 
     public void launchAboutModal() {
@@ -79,6 +137,30 @@ public class SciTool extends Application {
     }
 
     public static void main(String[] args) {
+        System.setProperty( "apple.laf.useScreenMenuBar", "true" );
+        System.setProperty("apple.awt.application.name","true");
+
+
+        Desktop desktop = Desktop.getDesktop();
+        if( desktop.isSupported( Desktop.Action.APP_ABOUT ) ) {
+            desktop.setAboutHandler( e -> {
+                // show about dialog
+            } );
+        }
+        if( desktop.isSupported( Desktop.Action.APP_PREFERENCES ) ) {
+            desktop.setPreferencesHandler( e -> {
+                // show preferences dialog
+            } );
+        }
+        if( desktop.isSupported( Desktop.Action.APP_QUIT_HANDLER ) ) {
+            desktop.setQuitHandler( (e, response) -> {
+                boolean canQuit = true;// TODO
+                if( canQuit )
+                    response.performQuit();
+                else
+                    response.cancelQuit();
+            } );
+        }
         launch();
     }
 
